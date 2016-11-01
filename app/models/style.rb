da79a2cb21c38408691c812931db902b051d5bf2
Brad <bgreen3@uoregon.edu>
2016-10-31 18:08:36 -0600

    adds tests for beer and style models

--- app/models/style.rb
+++ app/models/style.rb
@@ -1,2 +1,6 @@
 class Style < ActiveRecord::Base
+  has_many :beers
+
+  validates :name, presence: true, uniqueness: true
+
 end
--- spec/models/style_spec.rb
+++ spec/models/style_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Style, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validates attributes" do
+    scenario "style invalid without name" do
+      style = Style.new()
+
+      expect(style).to be_invalid
+    end
+
+    scenario "style valid with attributes present" do
+      style = Style.new(name: "Stout")
+
+      expect(style).to be_valid
+    end
+  end
 end
