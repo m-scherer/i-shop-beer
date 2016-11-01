65c82438957941aed5f29b4a629b6a6abeb4e27f
Nicholas Martinez <nicholasmartinez@gmail.com>
2016-11-01 10:26:57 -0600

    Beers index created with view, building out styles index with nested resource

---
+++ spec/features/beers/user_can_visit_beer_index_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Beer do
+  context "user experience on beer index page" do
+    scenario "a user can see beers displayed on the beer index page" do
+        style = Style.new(name: "IPA")
+        beer = Beer.create(name: "Pliny the Elder", style: style, price: 7.00 )
+
+        visit beers_path
+
+        expect(beer).to have_content("Pliny the Elder")
+    end
+  end
+end
