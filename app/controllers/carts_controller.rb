2d182d930b973ae2ba4f0a87287095a7300ee756
Michael Scherer <michael.c.scherer@gmail.com>
2016-11-01 12:02:23 -0600

    Add cart features test for adding beers to cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,11 @@
 class CartsController < ApplicationController
-  include ActionView::Helpers::TextHelper
 
-  
+  def create
+    beer = Beer.find(params[:beer_id])
+    @cart.add_beer(beer.id)
+    session[:cart] = @cart.contents
+    flash[:notice] = "#{beer.name} added to cart!"
+    redirect_to style_beers_path(beer.style)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
     resources :beers, only: [:index]
   end
 
+  resources :carts, only: [:create]
+
 end
