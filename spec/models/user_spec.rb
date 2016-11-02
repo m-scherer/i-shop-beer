7ee84736cfe475da3915bc807a63845b036e744e
Michael Scherer <michael.c.scherer@gmail.com>
2016-11-01 20:37:50 -0600

    Add User model testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "invalid attributes" do
+    it "is invalid without an email" do
+      invalid_user = User.new(password: "test")
+
+      expect(invalid_user).to be_invalid
+    end
+    it "is invalid without a password" do
+      invalid_user = User.new(email: "bob@test.com")
+
+      expect(invalid_user).to be_invalid
+    end
+  end
+
+  context "valid attributes" do
+    it "is valid" do
+      valid_user = User.new(email: "bob@test.com", password: "pass")
+
+      expect(valid_user).to be_valid
+    end
+  end
+
+  context "relationships" do
+    pending
+    end
+
 end
