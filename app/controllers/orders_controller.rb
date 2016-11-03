fd15b7380a95fae06288eeae37c47c12828f7dbb
Michael Scherer <michael.c.scherer@gmail.com>
2016-11-03 16:34:55 -0600

    Add quantity to the order page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,18 +3,12 @@ class OrdersController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     @order = @user.orders.new
-
-    if @order.save && create_beer_orders
+    if @order.save
+      @order.create_beer_orders(cart_beers, @cart)
       flash[:success] = "Placed your order!"
       redirect_to user_order_path(@user, @order)
     else
       redirect_to cart_path
-    end
-  end
-
-  def create_beer_orders
-    cart_beers.each do |beer|
-      @order.beer_orders.new(beer: beer).save
     end
   end
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,11 @@ class Order < ActiveRecord::Base
   belongs_to :user
   has_many :beer_orders
   has_many :beers, through: :beer_orders
+
+  def create_beer_orders(cart_beers, cart)
+    cart_beers.each do |beer|
+      beer_orders.new(beer: beer, quantity: cart.get_quantity_of_beer(beer)).save
+    end
+  end
+
 end
