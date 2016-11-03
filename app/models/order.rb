9f03b258ab643d08be5c27c12adada15123b663e
Michael Scherer <michael.c.scherer@gmail.com>
2016-11-03 10:12:28 -0600

    Add order model testing

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ActiveRecord::Base
   belongs_to :user
+  has_many :beer_orders
+  has_many :beers, through: :beer_orders
 end
--- spec/models/beer_order_spec.rb
+++ spec/models/beer_order_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe BeerOrder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'relationships' do
+    it "has one beer" do
+      beer_order = BeerOrder.new
+
+      expect(beer_order).to respond_to(:beer)
+      expect(beer_order).to_not respond_to(:beers)
+    end
+    it "has one order" do
+      beer_order = BeerOrder.new
+
+      expect(beer_order).to respond_to(:order)
+      expect(beer_order).to_not respond_to(:orders)
+    end
+  end
 end
