7e791d2383ae00c5817fa63555cdbf8c8b50a45c
Dave Junta <djunta@gmail.com>
2016-11-05 14:00:45 -0600

    admin functionality done

--- spec/features/admin/logged_in_admin_can_see_styles_index_spec.rb
+++ spec/features/admin/logged_in_admin_can_see_styles_index_spec.rb
@@ -1,28 +1,19 @@
 require 'rails_helper'
 
-describe  'As a logged in admin' do
-  context 'when I go to the admin categories path' do
-    it "I can see all the categories" do
+describe  'Can log in as admin' do
+  context 'when I go to the login page' do
+    it "takes me to admin page if user is an admin" do
 
+      user = User.create(email: "dj@dj.com", password: "password", role: 1)
 
+      visit "/login"
+      fill_in "email", with: user.email
+      fill_in "password", with: user.password
+      within(".form-horizontal") do
+        click_on "Login"
+      end
 
-
-
-      # style = Style.create(name: "IPA")
-      # beer = Beer.create(name: "Pallet Jack", style: style, price: 5.00)
-      # user = User.create(email: "brad@yahoo.com", password: "pass")
-      #
-      # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      #
-      # visit style_beers_path(style, beer)
-      # click_button "Add to Cart"
-      # visit cart_path
-      # click_on "Place Order"
-      #
-      # expect(current_path).to eq(user_order_path(user, Order.all.last))
-      # expect(page).to have_content(beer.name)
-      # expect(page).to have_content("Quantity: 1")
-      # expect(page).to have_content(beer.price)
-    end
+      expect(page).to have_content("Admin Dashboard")
     end
   end
+end
