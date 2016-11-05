5341903abd48d65141aeecee286573dbf0ebed57
Dave Junta <djunta@gmail.com>
2016-11-05 13:01:08 -0600

    added login tests

--- spec/features/admin/logged_in_admin_can_see_styles_index_spec.rb
+++ spec/features/admin/logged_in_admin_can_see_styles_index_spec.rb
@@ -7,21 +7,22 @@ describe  'As a logged in admin' do
 
 
 
-      
-      style = Style.create(name: "IPA")
-      beer = Beer.create(name: "Pallet Jack", style: style, price: 5.00)
-      user = User.create(email: "brad@yahoo.com", password: "pass")
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
-      visit style_beers_path(style, beer)
-      click_button "Add to Cart"
-      visit cart_path
-      click_on "Place Order"
-
-      expect(current_path).to eq(user_order_path(user, Order.all.last))
-      expect(page).to have_content(beer.name)
-      expect(page).to have_content("Quantity: 1")
-      expect(page).to have_content(beer.price)
+      # style = Style.create(name: "IPA")
+      # beer = Beer.create(name: "Pallet Jack", style: style, price: 5.00)
+      # user = User.create(email: "brad@yahoo.com", password: "pass")
+      #
+      # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      #
+      # visit style_beers_path(style, beer)
+      # click_button "Add to Cart"
+      # visit cart_path
+      # click_on "Place Order"
+      #
+      # expect(current_path).to eq(user_order_path(user, Order.all.last))
+      # expect(page).to have_content(beer.name)
+      # expect(page).to have_content("Quantity: 1")
+      # expect(page).to have_content(beer.price)
+    end
     end
   end
